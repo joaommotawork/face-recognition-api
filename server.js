132e3ab2727ed738aed01c30e04fdbbee54999a7
João Mota <johnymoty@users.noreply.github.com>
2020-04-03 03:04:39 +0100

    DB Heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.json(database.users)
+    res.json('It Is Working')
 })
 
 app.post('/signIn', signIn.handleSignIn(db, bcrypt))
