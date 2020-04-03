12742ab5776895bfb7f310fa42a01e0f4602dfa1
João Mota <johnymoty@users.noreply.github.com>
2020-04-03 03:23:45 +0100

    DB Heroku Fix

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'johnymoty',
-        password: '',
-        database: 'facerecognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 })
 
